95bd56a652e99bb0c30d35065b4f40bf5846368d
jingyan wang <wayenvan@outlook.com>
2023-03-09 17:41:43 +0000

    finish structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='csi_sign_language',
+    version='',
+    packages=['csi_sign_language', 'csi_sign_language.dataset'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='jingyan wang',
+    author_email='',
+    description=''
+)
+
