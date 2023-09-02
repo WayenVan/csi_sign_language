b24934df13b892df388fc5cf3f9adec15d29b50c
Wang Jingyan <wayenvan@outlook.com>
2023-09-02 23:04:10 +0100

    before leave

--- src/csi_sign_language/engines/trainers.py
+++ src/csi_sign_language/engines/trainers.py
@@ -4,16 +4,18 @@ from torch.nn import Module
 from torch.utils.data import DataLoader
 from logging import Logger
 from tqdm import tqdm
+from ..models.models import GNNUnet
+from torchmetrics.classification import Accuracy
 
 class Trainner():
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model: GNNUnet, optimizer: Optimizer, train_loader: DataLoader, loss_fn, _logger: Logger ) -> None:
 
-    def do_train(self, model: Module, optimizer: Optimizer, train_loader: DataLoader, loss_fn, _logger: Logger ):
+    def do_train(self, :
         model.train()
         logger = _logger.getChild(__class__.__name__)
         logger.info('start training')
+        accuracy = Accuracy(task='multiclass', num_class=)
         for data in tqdm(DataLoader):
             optimizer.zero_grad()
             x: torch.Tensor = torch.zeros(3,2)
